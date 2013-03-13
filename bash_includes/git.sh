67eba377b324446f1ef592e3ab40a7173c7cb61e
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-03-13 14:50:34 +0100

    Adjusted git env variable assignments to match m3 :)

--- bash_includes/git.sh
+++ bash_includes/git.sh
@@ -1,8 +1,8 @@
 ###########
 # GIT     #
 ###########
-export GIT_AUTHOR_NAME="Leon Weidauer"
-export GIT_AUTHOR_EMAIL="leon.weidauer@googlemail.com"
+export GIT_AUTHOR_NAME="Thorsten Schmitt-Rink"
+export GIT_AUTHOR_EMAIL="schmittrink@gmail.com"
 export GIT_COMMITER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITER_EMAIL="$GIT_AUTHOR_EMAIL"
 
