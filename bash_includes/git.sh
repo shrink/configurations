f5b10889d772925a918c88889d0a9d143bf0b58e
Leon Weidauer <leon.weidauer@googlemail.com>
2013-03-20 15:18:27 +0100

    fixed typo in git env variables

--- bash_includes/git.sh
+++ bash_includes/git.sh
@@ -3,6 +3,6 @@
 ###########
 export GIT_AUTHOR_NAME="Leon Weidauer"
 export GIT_AUTHOR_EMAIL="leon.weidauer@googlemail.com"
-export GIT_COMMITER_NAME="$GIT_AUTHOR_NAME"
-export GIT_COMMITER_EMAIL="$GIT_AUTHOR_EMAIL"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
