238c43beca02f994261d9ce1d6b2d494cd681352
Leon Weidauer <leon.weidauer@googlemail.com>
2013-04-09 11:19:03 +0200

    Aliases

--- bash_includes/aliases.sh
+++ bash_includes/aliases.sh
@@ -3,7 +3,21 @@
 ###########
 alias ll="ls -lG"
 alias la="ls -laG"
-alias ..="cd .."
-alias ...="cd ../.."
+
+# navigation to parent directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+alias .6='cd ../../../../../..'
+
+# create non-existing parent directories automatically
+alias mkdir='mkdir -pv'
+
+
 alias grep="grep --color"
 
+alias serve='python -m SimpleHTTPServer 8080'
